680f6ebf49dad8cf9c5195326d923c3b162b995a
calonsogarcia <carolina.alon.garcia@gmail.com>
2021-09-29 19:25:04 +0200

    Movies route created

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get('/', (req, res, next) => 
+    res.render('index'));
+
+router.get("/movies", (req, res, next) => {
+    res.render('movies.hbs')
+})
 
 module.exports = router;
